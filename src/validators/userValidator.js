1284308b5f54cb37617b44b81730958ada108f1f
sayanseksenbaev <sayan123serv@gmail.com>
2025-03-06 20:13:52 +0500

    fix: updated new user model

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -1,11 +1,13 @@
 const { body } = require('express-validator');
 
 exports.registerValidator = [
-  body('name').notEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Invalid email'),
   body('password')
     .isLength({ min: 6 })
     .withMessage('Password must be at least 6 characters'),
+  body('confirmPassword')
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage('Passwords do not match'),
 ];
 
 exports.loginValidator = [
